d32ebb7575376bc3eb9714052d2ed45b377954b8
mates71 <musaates71@gmail.com>
2019-03-02 18:59:55 -0500

    target added

--- target-cucumber/report.js
+++ target-cucumber/report.js
@@ -39,21 +39,21 @@ formatter.match({
   "location": "WeaterStepDefn.weater_home_page()"
 });
 formatter.result({
-  "duration": 2945144767,
+  "duration": 1824742183,
   "status": "passed"
 });
 formatter.match({
   "location": "WeaterStepDefn.user_search_weater_in_specific_city()"
 });
 formatter.result({
-  "duration": 15245490630,
+  "duration": 15592184584,
   "status": "passed"
 });
 formatter.match({
   "location": "WeaterStepDefn.user_able_to_get_result()"
 });
 formatter.result({
-  "duration": 1064229,
+  "duration": 2313428,
   "error_message": "cucumber.api.PendingException: TODO: implement me\n\tat chase.stepDefns.WeaterStepDefn.user_able_to_get_result(WeaterStepDefn.java:73)\n\tat ✽.Then user able to get result(weater.feature:7)\n",
   "status": "pending"
 });
